d4ad30afe39623368dc09de913488b07a54be5fa
ingrid10052 <ingridpartida2001@gmail.com>
2021-10-07 11:19:40 -0500

    Made some things, expos and home view
    jeje xD

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,28 @@
 <template>
-    <div class="container">
-        <h1>home</h1>
+    <div class="container" style="background: #F4EDE7">
+        <h3>Bienvenido como te llames</h3>
+        <h1 class="font-weight-bold">#MuseoDeTodos</h1>
+        <div class="row">
+            <div class="col-4">
+                <img src="logo.png" class="img-fluid">
+                <div class="d-grid gap-2">
+                    <a class="btn btn-dark" href="http://localhost:8080/about" role="button" >Administrar colaboradores</a>
+                    <a class="btn btn-dark" href="http://localhost:8080/expos" role="button">Administrar exposiciones</a>
+                </div>
+            </div>
+            <div class="col-8" style="background: red;">
+                <h2 class="text-center">Exposiciones del mes</h2>
+                <div class="row-2" style="background: pink;">
+                        <h3 class="text-center"> MES </h3>
+                        <div class="row-2" style="background: white;">
+                            <h3 class="text-center"> expo numero xD</h3>
+                        </div>
+                </div>
+                <div class="row" style="background: green;" >
+                        <h3 class="text-center"> Menú de la semana: </h3>
+                </div>
+            </div>
+        </div>      
     </div>
 </template>
 
